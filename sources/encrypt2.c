6ec864d9ddf40fa746c05a5b380884453588cf4d
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-10-30 13:43:39 +0100

    Update encrypt2.c

--- sources/encrypt2.c
+++ sources/encrypt2.c
@@ -1,1 +1,14 @@
 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+#define AND &
+#define XOR ^
+#define NOT !
+
+int i = 0;
